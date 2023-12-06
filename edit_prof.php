f79de23b74156646c81617ff2807054e261f0d9f
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-06 22:28:55 +0530

    Update edit_prof.php

--- edit_prof.php
+++ edit_prof.php
@@ -2,10 +2,10 @@
 session_start();
 
 // Database connection details
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "user1_db";
+$servername = "sqlserver43.mysql.database.azure.com";
+$username = "nirupamashree";
+$password = "laks@2003";
+$database = "user1_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
