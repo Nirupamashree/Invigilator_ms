f583422af2291e588526aa4fff3d54cb27448e45
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-06 22:36:36 +0530

    Update register_form.php

--- register_form.php
+++ register_form.php
@@ -112,10 +112,11 @@
 
 <?php
 // Database connection
-$host = 'localhost';
+
+$host = 'sqlserver43.mysql.database.azure.com';
 $db = 'user1_db';
-$user = 'root';
-$password = '';
+$user = 'nirupamashree';
+$password = 'laks@2003';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
