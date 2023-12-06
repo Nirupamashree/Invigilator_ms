e6b3f4838811f24189f87358d2071460ac1de853
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-06 23:06:23 +0530

    Update swap_request.php

--- swap_request.php
+++ swap_request.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
     $servername = "sqlserver43.mysql.database.azure.com";
     $username = "nirupamashree";
     $password = "laks@2003";
-    $database = "user1_db";
+    $dbname = "user1_db";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
