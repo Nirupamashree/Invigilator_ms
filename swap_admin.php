61349f3452cd220cef25536b8b3728c8307f2e55
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-06 22:31:52 +0530

    Update swap_admin.php

--- swap_admin.php
+++ swap_admin.php
@@ -1,10 +1,10 @@
 <?php
 session_start(); // Start the session
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "user1_db";
+$servername = "sqlserver43.mysql.database.azure.com";
+$username = "nirupamashree";
+$password = "laks@2003";
+$database = "user1_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
