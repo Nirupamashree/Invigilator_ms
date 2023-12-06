535e90a35b23e3893e28531e28b4b1453110baf5
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-06 22:28:08 +0530

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -1,8 +1,8 @@
 <?php
 // MySQL server configuration
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "sqlserver43.mysql.database.azure.com";
+$username = "nirupamashree";
+$password = "laks@2003";
 $database = "user1_db";
 
 // Create a connection to the MySQL server
