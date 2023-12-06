853acd1be55af14c3c462a76c196272479c20fde
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-06 22:32:24 +0530

    Update swap_request.php

--- swap_request.php
+++ swap_request.php
@@ -2,10 +2,10 @@
 session_start(); // Start the session
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "user1_db";
+    $servername = "sqlserver43.mysql.database.azure.com";
+    $username = "nirupamashree";
+    $password = "laks@2003";
+    $database = "user1_db";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
