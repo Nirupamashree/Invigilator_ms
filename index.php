c114c774194a489142120d0bb42c7e00715e0a3e
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-10 01:54:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
                     <div class="swiper-wrapper">
                         <!--==================== DISCOVER 1 ====================-->
                         <div class="discover__card swiper-slide">
-                            <img src="image4.jpg" alt="" class="discover__img">
+                            <img src="image4.jpeg" alt="" class="discover__card-1">
                             <div class="discover__data">
                                 <h2 class="discover__title"></h2>
                                 <span class="discover__description"></span>
@@ -131,7 +131,7 @@
 
                         <!--==================== DISCOVER 2 ====================-->
                         <div class="discover__card swiper-slide">
-                            <img src="image5.png" alt="" class="discover__img">
+                            <img src="image5.png" alt="" class="discover__card-2">
                             <div class="discover__data">
                                 <h2 class="discover__title">Personalization</h2>
                                 <span class="discover__description"></span>
@@ -140,7 +140,7 @@
 
                         <!--==================== DISCOVER 3 ====================-->
                         <div class="discover__card swiper-slide">
-                            <img src="image6.jpg" alt="" class="discover__img">
+                            <img src="image6.jpeg" alt="" class="discover__card-3">
                             <div class="discover__data">
                                 <h2 class="discover__title">Functionality</h2>
                                 <span class="discover__description"></span>
