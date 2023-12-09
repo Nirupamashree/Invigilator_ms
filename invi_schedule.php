931fa7252d86646eb888132363322dfc44f96938
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-10 01:04:03 +0530

    Update invi_schedule.php

--- invi_schedule.php
+++ invi_schedule.php
@@ -100,7 +100,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["allocate"])) {
         $servername = "sqlserver43.mysql.database.azure.com";
         $username = "nirupamashree";
-        $password = "laks@2003";
+        $password = "password@123";
         $dbname = "user1_db";
 
         // Create connection
