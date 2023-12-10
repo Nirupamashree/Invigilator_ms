aecc083417c1a642905ba607d7c8f8686462401b
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-10 14:23:25 +0530

    Update f_Table1.php

--- f_Table1.php
+++ f_Table1.php
@@ -143,44 +143,46 @@
 
 </head>
 <body>
-    <?php
-    // Database connection
-    $host = 'sqlserver43.mysql.database.azure.com';
-    $db = 'user1_db';
-    $user = 'nirupamashree';
-    $password = 'password@123';
-    $charset = 'utf8mb4';
+<?php
+// Database connection
+$host = 'sqlserver43.mysql.database.azure.com';
+$db = 'user1_db';
+$user = 'nirupamashree';
+$password = 'password@123';
+$charset = 'utf8mb4';
 
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+} catch (PDOException $e) {
+    die("Error: " . $e->getMessage());
+}
+
+$showTable = false;
+$facultyName = $_GET['faculty_name'] ?? '';
+
+if (!empty($facultyName)) {
+    // Wrap the query execution in a try-catch block to handle potential errors
     try {
-        $pdo = new PDO($dsn, $user, $password, $options);
+        $stmt = $pdo->prepare("SELECT * FROM $facultyName");
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $showTable = true;
     } catch (PDOException $e) {
-        die("Error: " . $e->getMessage());
+        echo '<script>showAlert("Error fetching data: ' . $e->getMessage() . '");</script>';
     }
+}
 
-    $showTable = false;
-    $facultyName = $_GET['faculty_name'] ?? '';
-
-    if (!empty($facultyName)) {
-        if ($pdo->query("SHOW TABLES LIKE '$facultyName'")->rowCount() > 0) {
-            $stmt = $pdo->prepare("SELECT * FROM $facultyName");
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            $showTable = true;
-        }
-    }
-
-    if (!$showTable && !empty($facultyName)) {
-        echo '<script>showAlert("Faculty TimeTable does not exist.");</script>';
-    }
-    ?>
-
+if (!$showTable && !empty($facultyName)) {
+    echo '<script>showAlert("Faculty TimeTable does not exist.");</script>';
+}
+?>
     <div class="container">
         <h2>FACULTY TIMETABLE</h2>
         <div class="form-container">
