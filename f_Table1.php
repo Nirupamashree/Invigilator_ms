9fb8668c2b0fab85f7e39b90278f2854f38903ad
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-10 12:18:25 +0530

    Update f_Table1.php

--- f_Table1.php
+++ f_Table1.php
@@ -3,9 +3,110 @@
 <head>
     <title>Faculty Table Viewer</title>
     <style>
-        /* Your existing CSS styles remain unchanged */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f7f7f7;
+            margin: 0;
+            padding: 0;
+        }
+
+        h2 {
+            color: #333;
+            text-align: center;
+            margin-top: 30px;
+            font-size: 24px;
+            font-weight: bold;
+            /*text-transform: uppercase;*/
+            letter-spacing: 1px;
+            padding-bottom: 10px;
+            border-bottom: 1px solid #ddd;
+        }
+
+        table {
+            margin: 0 auto;
+            border-collapse: separate;
+            border-spacing: 0;
+            width: 80%;
+            background-color: #fff;
+            margin-top: 30px;
+            border-radius: 8px;
+            overflow: hidden;
+            box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.2); /* Modified shadow */
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+            border-right: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #f2f2f2;
+            font-weight: bold;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        .container::after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+
+        .form-container {
+            float: left;
+            width: 300px;
+            margin-right: 20px;
+        }
+
+        .form-container label {
+            font-weight: bold;
+            color: #555;
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        .form-container input[type="text"] {
+            width: 100%;
+            height: 30px;
+            margin-bottom: 10px;
+            padding: 5px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+            box-sizing: border-box;
+        }
+
+        .form-container input[type="button"] {
+            width: 100%;
+            height: 40px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+
+        .form-container input[type="button"]:hover {
+            background-color: #45a049;
+        }
+
+        .table-container {
+            margin-top: 30px;
+        }
+
+        .table-container table {
+            width: 100%;
+        }
+
+        .form-container input[type="button"] {
+            margin-bottom: 10px;
+        }
     </style>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
     <script>
         function showAlert(message) {
             alert(message);
@@ -13,13 +114,13 @@
 
         function showTable() {
             var facultyName = document.getElementById('faculty_name').value;
-            window.location.href = 'show-and-download.php?faculty_name=' + encodeURIComponent(facultyName);
+            window.location.href = '?faculty_name=' + encodeURIComponent(facultyName);
         }
     </script>
 </head>
 <body>
     <?php
-        // Database connection
+    // Database connection
     $host = 'sqlserver43.mysql.database.azure.com';
     $db = 'user1_db';
     $user = 'nirupamashree';
@@ -53,7 +154,7 @@
 
     if (!$showTable && !empty($facultyName)) {
         echo '<script>showAlert("Faculty TimeTable does not exist.");</script>';
-    } 
+    }
     ?>
 
     <div class="container">
@@ -61,7 +162,7 @@
         <div class="form-container">
             <label for="faculty_name">Faculty Name:</label>
             <input type="text" id="faculty_name" name="faculty_name" required value="<?php echo $facultyName; ?>">
-            <input type="button" onclick="showTable()" value="Show Table and Download PDF">
+            <input type="button" onclick="showTable()" value="Show Table">
             <br>
             <br>
         </div>
