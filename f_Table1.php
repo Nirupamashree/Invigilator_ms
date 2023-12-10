51b5ea8d72608f07bd8c9166e1bd26264c850936
Nirupamashree <136173960+Nirupamashree@users.noreply.github.com>
2023-12-10 18:45:57 +0530

    Update f_Table1.php

--- f_Table1.php
+++ f_Table1.php
@@ -160,6 +160,8 @@
 
     <div class="container">
         <h2>FACULTY TIMETABLE</h2>
+        <h1>Faculty time-table</h1>
+        <img src="tt.jpg" alt="Timetable Image" style="max-width: 100%; height: auto; margin-top: 20px;">
         <div class="form-container">
             <label for="faculty_name">Faculty Name:</label>
             <input type="text" id="faculty_name" name="faculty_name" required value="<?php echo $facultyName; ?>">
@@ -186,8 +188,6 @@
                 <?php endforeach; ?>
             </table>
         </div>
-        <h1>Faculty time-table</h1>
-        <img src="tt.jpg" alt="Timetable Image" style="max-width: 100%; height: auto; margin-top: 20px;">
         <?php endif; ?>
     </div>
 </body>
